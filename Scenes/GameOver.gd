76c052bad0e317456eab4411874362ea7f3ce06b
ejmondragon <edward.mondragon@tamu.edu>
2019-01-24 01:59:53 -0600

    Fixed bug where To Map button still showed on Game Over

--- Scenes/GameOver.gd
+++ Scenes/GameOver.gd
@@ -7,4 +7,5 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 func _on_Organism_died(org):
+	get_tree().get_root().get_node("Control/Canvas_UI/modeSwitch").hide()
 	$Turn.text += String(get_parent().get_node("Organism").died_on_turn)
