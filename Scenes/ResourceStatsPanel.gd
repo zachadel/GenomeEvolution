0cd1fb9434c1a17df8c15fe87c503b0dc95386c3
ejmondragon <edward.mondragon@tamu.edu>
2019-02-14 02:30:05 -0600

    Resources UI now slides well. Tree created. Tree population next.

--- Scenes/ResourceStatsPanel.gd
+++ Scenes/ResourceStatsPanel.gd
@@ -1,7 +1,8 @@
 extends Panel
 
 var root
-
+onready var tween_node = $Tween
+var hidden = true
 var player
 
 func _ready():
@@ -15,3 +16,14 @@ func set_player(_player):
 func set_up_tree():
 	var resource1 = $Tree.create_item(root)
 	resource1.set_text(0, "Resource 1:")
+
+func _on_Button_pressed():
+	if hidden:
+		tween_node.interpolate_property(self, "rect_position", get_position(), Vector2(0, 0), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+		tween_node.start()
+		hidden = false
+	else:
+		tween_node.interpolate_property(self, "rect_position", get_position(), Vector2(-250, 0), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+		tween_node.start()
+		hidden = true
+	print(hidden)
