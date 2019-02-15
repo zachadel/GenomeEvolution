52b138f4754e801ad1deb7d3cd9b3dabef3c6e4a
Edward Mondragon <insomnawolf@gmail.com>
2019-02-14 23:05:06 -0600

    Transferring work from surface to desktop. Working on populating resource tree.

--- Scripts/ResourceStatsPanel.gd
+++ Scripts/ResourceStatsPanel.gd
@@ -7,13 +7,21 @@ var player
 
 func _ready():
 	root = $Tree.create_item()
-	root.set_text(0, "ROOT")
+	root.set_text(0, "Tile Resources")
 	
 func set_player(_player):
 	player = _player
 	set_up_tree()
 
 func set_up_tree():
+	var res_tree = [[],[],[],[]]
+	
+	for i in range(0, 4):
+		var group = $Tree.create_item(root)
+		for j in range(0, 10):
+			var sub_group = $Tree.create_item(group)
+			#moreAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+	
 	var resource1 = $Tree.create_item(root)
 	resource1.set_text(0, "Resource 1:")
 
