5892f7478cca169897dd98569c7eae71bc6c515b
Edward Mondragon <insomnawolf@gmail.com>
2019-03-28 13:02:12 -0500

    Hover values now display resources.

--- Scenes/resources_grid.gd
+++ Scenes/resources_grid.gd
@@ -1,13 +1,24 @@
 extends Panel
 
+var org
+
 func _ready():
+	org = get_tree().get_root().get_node("Control/Canvas_CardTable/CardTable/Organism")
+	
 	$GridContainer/TextureProgress1/Label.hide()
 	$GridContainer/TextureProgress2/Label.hide()
 	$GridContainer/TextureProgress3/Label.hide()
 	$GridContainer/TextureProgress4/Label.hide()
 
 func _process(delta):
-	pass
+	
+	for i in range(1, 5):
+		var path = "GridContainer/TextureProgress" + str(i) + "/Label"
+		get_node(path).text = str(org.resources[i - 1])
+#		$GridContainer/TextureProgress1/Label.text = ""
+#		$GridContainer/TextureProgress2/Label.text = ""
+#		$GridContainer/TextureProgress3/Label.text = ""
+#		$GridContainer/TextureProgress4/Label.text = ""
 
 
 func _on_TextureProgress1_mouse_entered():
