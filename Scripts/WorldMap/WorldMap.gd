931fac04a0b12cf509c4b2b648a8d0afee9132cb
Alxl <44034025+ItsAlxl@users.noreply.github.com>
2020-02-20 12:59:41 -0600

    End-of-turn Softlock Fix

--- Scripts/WorldMap/WorldMap.gd
+++ Scripts/WorldMap/WorldMap.gd
@@ -283,6 +283,7 @@ func _input(event):
 		get_tree().set_input_as_handled()
 
 func enable_camera():
+	$MapCamera.zoom = Vector2(1, 1);
 	$MapCamera.make_current()
 
 #Enter the use case as an int from Game.PLAYER_VIEW
