e75c07313071b47bc0631e3a8ac8a157cc774858
RyanMalt <34022706+RyanMalt@users.noreply.github.com>
2020-10-12 17:02:50 -0500

    Applied fixes for issues #90 and #93

--- Scripts/Globals/Actions.gd
+++ Scripts/Globals/Actions.gd
@@ -4,11 +4,11 @@ var env = {}
 
 func _ready():
 	var contents = load_file('res://env.json')
-	env.parse_json(contents)
+	env = parse_json(contents)
 
 func load_file(file):
 	var f = File.new()
 	f.open(file, File.READ)
 	var contents = f.get_as_text()
-	file.close()
+	f.close()
 	return contents
