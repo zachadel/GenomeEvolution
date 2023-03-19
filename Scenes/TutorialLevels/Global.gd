5e6ebc9e6815045f38d731ed72007014a7ce9a4d
Ethan Kim <ethankim9091@gmail.com>
2023-03-19 08:51:01 -0500

    created tutorial player enemy map

--- Scenes/TutorialLevels/Global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var node_creation_parent = null
-
-func instance_node(node, location, parent):
-	var node_instance = node.instance()
-	parent.add_child(node_instance)
-	node_instance.global_position = location
-	return node_instance
--- Scenes/TutorialLevels/MiniGame.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	TutorialGlobal.node_creation_parent = self
-	
-func _exit_tree():
-	TutorialGlobal.node_creation_parent = null
